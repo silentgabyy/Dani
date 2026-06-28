21cd01834209e101336856f394a1c905607cc3a1
silentgabyy <carlosgabriel3309@gmail.com>
2026-06-28 02:31:47 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "How Sweet- NewJeans",
+        name: "PUBG",
         // Activity type number (0 = Playing).
-        type: 2,
+        type: 0,
       },
     ],
   },
