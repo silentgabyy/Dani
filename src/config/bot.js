cf9373c136b45fefb1b91998b04d15405ea70a3b
silentgabyy <carlosgabriel3309@gmail.com>
2026-06-28 02:25:15 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "How Sweet- NewJeans",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 2,
       },
     ],
   },
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#5D87C7",
+      secondary: "#95B6E6",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
